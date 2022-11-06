6bb7693d15235f2f2e7eb8e372040f37d2d111d8
RafaelTKM <rafalkirst96@oulook.com>
2022-11-06 20:00:55 -0300

    adicionando jogo 1

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
+  createCard("20/11", "domingo", createGame("qatar", "13:00", "ecuador")) +
   createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
   createCard(
     "28/11",
